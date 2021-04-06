da2c2e4156ad931b263dbcc114b816b25d293382
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-06 17:56:44 -0400

    Update feature and model tests for apply for pets

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,43 +1,90 @@
 require 'rails_helper'
 
 RSpec.describe "the Application show page" do
-  it "should display applicant info" do
-    applicant = Application.find_or_create_by!(name: "Fred",
+  before(:each) do
+    @applicant = Application.find_or_create_by!(name: "Fred",
                               street_address: "123 way",
                               city: "Denver",
                               state: "Colorado",
                               zip_code: 80204,
-                              description: "I like dogs",
                               status: "In Progress",
                               created_at: Time.now,
                               updated_at: Time.now
                               )
-    visit "/applications/#{applicant.id}"
+  @pound = Shelter.find_or_create_by!(foster_program: true,
+                                    name: "da pound",
+                                    city: "Denver",
+                                    rank: 1,
+                                    created_at: Time.now,
+                                    updated_at: Time.now
+                                    )
+  @dog = @pound.pets.find_or_create_by!(name: "Bob",
+                                      adoptable: true,
+                                      age: 1,
+                                      breed: "smol",
+                                      created_at: Time.now,
+                                      updated_at: Time.now
+                                      )
 
-    expect(page).to have_content(applicant.name)
-    expect(page).to have_content(applicant.street_address)
-    expect(page).to have_content(applicant.city)
-    expect(page).to have_content(applicant.state)
-    expect(page).to have_content(applicant.zip_code)
-    expect(page).to have_content(applicant.description)
+    end
+
+    it "should display applicant info" do
+    visit "/applications/#{@applicant.id}"
+
+    expect(page).to have_content(@applicant.name)
+    expect(page).to have_content(@applicant.street_address)
+    expect(page).to have_content(@applicant.city)
+    expect(page).to have_content(@applicant.state)
+    expect(page).to have_content(@applicant.zip_code)
   end
 
   it "shows links that can be clicked on" do
-    applicant = Application.find_or_create_by!(name: "Fred",
-                              street_address: "123 way",
-                              city: "Denver",
-                              state: "Colorado",
-                              zip_code: 80204,
-                              description: "I like dogs",
-                              status: "In Progress",
-                              created_at: Time.now,
-                              updated_at: Time.now
-                              )
-    visit "/applications/#{applicant.id}"
+    visit "/applications/#{@applicant.id}"
 
     click_link "Pets"
     click_link "Shelters"
     click_link "Veterinarians"
     click_link "Veterinary Offices"
+  end
+
+  it 'gives message to add pet before submittal description box and status in progress' do
+    visit "/applications/#{@applicant.id}"
+
+    expect(page).to have_content("Please add an animal to your application to submit")
+    expect(page).to have_content(@applicant.default)
+  end
+
+  it 'can search for pets to add and can add them' do
+    visit "/applications/#{@applicant.id}"
+
+    expect(page).to have_content("Add a pet to this application by name:")
+
+    fill_in "search", with: "bob"
+    click_button "Submit"
+
+    expect(current_path).to eq("/applications/#{@applicant.id}")
+    expect(page).to have_content("Bob")
+    expect(page).to have_button("Adopt this pet")
+
+    click_button "Adopt this pet"
+
+    expect(current_path).to eq("/applications/#{@applicant.id}")
+    expect(page).to have_content("Bob") #How to do a within for this?
+    expect(page).to have_content("Please describe below why you'd make a good home for this pet:")
+  end
+
+  it 'can submit an application' do
+    visit "/applications/#{@applicant.id}"
+
+    fill_in "search", with: "bob"
+    click_button "Submit"
+    click_button "Adopt this pet"
+
+    fill_in "description", with: "I like doges"
+    click_button "Submit Application"
+
+    expect(current_path).to eq("/applications/#{@applicant.id}")
+    expect(page).to have_content("You've already submitted your application")
+    expect(page).to have_content(@applicant.pending_status)
   end
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,34 @@ RSpec.describe Application, type: :model do
     it { should have_many :application_pets }
     it { should have_many(:pets).through(:application_pets) }
   end
+
+  describe 'instance-methods' do
+    describe 'default status' do
+      it 'returns the status of in progress by default' do
+        applicant = Application.find_or_create_by!(name: "Fred",
+                                  street_address: "123 way",
+                                  city: "Denver",
+                                  state: "Colorado",
+                                  zip_code: 80204,
+                                  status: "In Progress"
+                                  )
+
+        expect(applicant.default).to eq("In Progress")
+      end
+    end
+
+    describe 'updated status after application submittal' do
+      it 'changes the application stauts to pending' do
+        applicant = Application.find_or_create_by!(name: "Fred",
+                                  street_address: "123 way",
+                                  city: "Denver",
+                                  state: "Colorado",
+                                  zip_code: 80204,
+                                  status: "In Progress"
+                                  )
+
+        expect(applicant.pending_status).to eq("Pending")
+      end
+    end
+  end
 end
