ffc3c72a0510f52a113f0bc4b30cb96e39d97164
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-08 22:32:47 -0400

    Make update in admin appliactions controller and redo view for approved applications

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,4 +1,17 @@
 class Admin::ApplicationsController < ApplicationController
   def show
-  end 
+    @applicant = Application.find(params[:id])
+    @pets = Application.find(params[:id]).pets
+  end
+
+  def update
+    @pets = Application.find(params[:id]).pets
+    @applicant = Application.find(params[:id])
+    @joins = ApplicationPet.where("application_id = ?", params[:id])
+    @application_pets = @joins.update(
+      status: 0
+    )
+
+    render :show
+  end
 end
