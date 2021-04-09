6ca319b90c8b738a6029e36b6a1f9507721fb4a6
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-08 20:33:05 -0400

    Add shelters to routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
     resources :shelters, :applications
   end
 
+  get '/shelters', to: 'shelters#index'
+  get '/shelters/new', to: 'shelters#new'
+  get '/shelters/:id', to: 'shelters#show'
+  post '/shelters', to: 'shelters#create'
+  get '/shelters/:id/edit', to: 'shelters#edit'
+  patch '/shelters/:id', to: 'shelters#update'
+  delete '/shelters/:id', to: 'shelters#destroy'
+
   get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show'
   get '/pets/:id/edit', to: 'pets#edit'
